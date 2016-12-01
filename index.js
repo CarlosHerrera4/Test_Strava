970acc8026829fbb777cb970ffdb561cad27e5aa
MundoGister <mundogister@gmail.com>
2016-12-01 19:33:28 +0100

    Add package.json

--- index.js
+++ index.js
@@ -1,4 +1,11 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const app = express();
+const port = process.env.PORT || 3000 ;
+
 var strava = require('strava-v3');
+
 strava.athlete.listRoutes({
 	"id":"",
 	"access_token": "",
@@ -12,14 +19,17 @@ strava.athlete.listRoutes({
 	console.log('Venga va');
 
 });
-strava.activities.get({
-	"id":"",
-	"access_token": ""
-}, function(err, payload) {
-	if (err) {
-		console.log(err);
-	} else {
-		console.log(payload);
-	}
-	console.log('Venga va');
+
+app.listen(port, function() {
+	strava.activities.get({
+		"id":"",
+		"access_token": ""
+	}, function(err, payload) {
+		if (err) {
+			console.log(err);
+		} else {
+			console.log(payload);
+		}
+		console.log('Venga va');
+	});
 });
