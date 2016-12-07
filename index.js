87ad6e0372af595099fb57444c70adb875104784
MundoGister <mundogister@gmail.com>
2016-12-07 19:54:51 +0100

    Add heroku

--- index.js
+++ index.js
@@ -27,29 +27,31 @@ const strava = require('strava-v3');
 
 app.get('/actividad', (req, res) => {
 	strava.activities.get({
-		"id":"8519415",
+		"id":"750681983",
 		"access_token": "4005e5f9913ce8fcdf45ec1cb3a5b383f23c6ef3"
 	}, function(err, payload) {
-		console.log(payload)
+		//console.log(payload)
 		//console.log(err)
 		//console.log('Venga va');
-		res.send(200, payload)
+		res.status(200).send(payload)
+		//res.send(200, payload)
 	});
 })
 
 app.listen(port, function() {
-	strava.activities.get({
-		"id":"8519415",
-		"access_token": "4005e5f9913ce8fcdf45ec1cb3a5b383f23c6ef3"
-	}, function(err, payload) {
-		console.log(payload)
-		console.log(err)
-		// if (err) {
-		// 	console.log(err);
-		// } else {
-		// 	console.log(payload);
-		// }
-		console.log('Venga va');
-	});
-	console.log("oleeeee")
+	console.log(`API REST corriendo en http://localhost:${port}`)
+	// strava.activities.get({
+	// 	"id":"750681983",
+	// 	"access_token": "4005e5f9913ce8fcdf45ec1cb3a5b383f23c6ef3"
+	// }, function(err, payload) {
+	// 	console.log(payload)
+	// 	console.log(err)
+	// 	// if (err) {
+	// 	// 	console.log(err);
+	// 	// } else {
+	// 	// 	console.log(payload);
+	// 	// }
+	// 	console.log('Venga va');
+	// });
+	// console.log("oleeeee")
 });
